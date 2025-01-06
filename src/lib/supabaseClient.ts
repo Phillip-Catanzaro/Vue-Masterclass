b555fd25f4e17d26bad4e107f4679edad418bdfd
Phillip-Catanzaro <136368586+Phillip-Catanzaro@users.noreply.github.com>
2025-01-06 16:21:44 -0500

    git ignore

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-  'https://zjrzldajzkeozzpfqivo.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpqcnpsZGFqemtlb3p6cGZxaXZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYxOTcxMjIsImV4cCI6MjA1MTc3MzEyMn0.WSnZ5U37pRqFfQ8Hb08qIO36MJrN8DtrX6rp4qY4Uec'
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
 )
